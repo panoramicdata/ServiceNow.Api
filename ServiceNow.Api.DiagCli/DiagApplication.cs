47a67d5c01adeb1d8aaef48ba358b9163f0d07b5
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2019-07-19 11:25:32 +0100

    Added initial diags

---
+++ ServiceNow.Api.DiagCli/DiagApplication.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ServiceNow.Api.DiagCli.Exceptions;
+using ServiceNow.Api.DiagCli.Models;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ServiceNow.Api.DiagCli
+{
+	internal class DiagApplication
+	{
+		private readonly ILogger<DiagApplication> _logger;
+		private readonly Configuration _configuration;
+		private readonly IServiceProvider _serviceProvider;
+
+		public DiagApplication(
+			ILogger<DiagApplication> logger,
+			IOptions<Configuration> options,
+			IServiceProvider serviceProvider)
+		{
+			_logger = logger;
+			_configuration = options.Value;
+			_serviceProvider = serviceProvider;
+		}
+
+		public async Task<int> RunAsync(string[] args)
+		{
+			_logger.LogInformation($"ServiceNow API Diagnostics v{ThisAssembly.AssemblyFileVersion}");
+
+			try
+			{
+				_configuration.Validate();
+			}
+			catch (ConfigurationException e)
+			{
+				_logger.LogError(e.Message);
+				return ExitCode.ConfigurationError;
+			}
+
+			var overallStopWatch = Stopwatch.StartNew();
+			_logger.LogInformation("Starting run...");
+
+			foreach (var test in _configuration.Tests)
+			{
+				await ExecuteTestAsync(test).ConfigureAwait(false);
+			}
+
+			_logger.LogInformation($"Run complete after {overallStopWatch.Elapsed.TotalSeconds:0.00}s.");
+			return ExitCode.Success;
+		}
+
+		private async Task ExecuteTestAsync(DiagnosticTest test)
+		{
+			IDiagnostic diagnostic;
+			switch (test.Type)
+			{
+				case DiagnosticType.Paging:
+					diagnostic = _serviceProvider.GetRequiredService<PagingDiagnostic>();
+					break;
+				default:
+					throw new NotSupportedException($"Test type {test.Type} is not supported.");
+			}
+
+			await diagnostic.ExecuteAsync(test).ConfigureAwait(false);
+		}
+	}
+}
---
+++ ServiceNow.Api.DiagCli/IDiagnostic.cs
@@ -0,0 +1,10 @@
+﻿using ServiceNow.Api.DiagCli.Models;
+using System.Threading.Tasks;
+
+namespace ServiceNow.Api.DiagCli
+{
+	internal interface IDiagnostic
+	{
+		Task ExecuteAsync(DiagnosticTest test);
+	}
+}
