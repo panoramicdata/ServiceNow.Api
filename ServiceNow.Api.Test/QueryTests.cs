8c543380df5e1826be0551afa37ab4697ae8fca6
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2019-07-19 11:35:48 +0100

    Added code missing for 1.1.20

--- ServiceNow.Api.Test/QueryTests.cs
+++ ServiceNow.Api.Test/QueryTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using ServiceNow.Api.Tables;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +10,54 @@ namespace ServiceNow.Api.Test
 {
 	public class QueryTests : ServiceNowTest
 	{
+		public QueryTests(ITestOutputHelper output) : base(output, "appsettings.ntt.dev.json", new Options { ValidateCountItemsReturned = true })
+		{
+		}
+
 		[Fact]
-		public async Task PagingTestAsync()
+		public async Task InternalPagingTestAsync()
 		{
 			// This test fails if not ordering by ORDERBYsys_id
 			const string query = null;
 			var fieldList = new List<string> { "sys_id" };
 			const string extraQueryString = null;
-			const int pageSize = 1000;
+			const int pageSize = 10000;
 
-			var result = await Client.GetAllByQueryInternalAsync<JObject>("u_ci_property", query, fieldList, extraQueryString, pageSize, default).ConfigureAwait(false);
+			var result = await Client.GetAllByQueryInternalJObjectAsync("u_ci_property", query, fieldList, extraQueryString, pageSize, default).ConfigureAwait(false);
 			Assert.NotNull(result);
 			Assert.NotEmpty(result);
+			Assert.True(result[0].ContainsKey("sys_id"));
+			// Not expecting the u_value field to be present as we asked for sys_id only
+			Assert.False(result[0].ContainsKey("u_value"));
+
+			// There should only be the properties requested, even though sys_id and sys_created_on are used internally for paging
+			var actualProperties = result[0].Properties().Select(p => p.Name).ToList();
+			Assert.Equal(fieldList.OrderBy(name => name), actualProperties.OrderBy(name => name));
+
+			// Check for dupes
+			var dupes = result.GroupBy(ci => ci["sys_id"]).Where(g => g.Count() > 1).Select(g => new { Id = g.First()["sys_id"], Count = g.Count() }).ToList();
+
+			var unique = result.GroupBy(ci => ci["sys_id"]).Select(ci => ci.First()).ToList();
+
+			Logger.LogInformation($"Found {dupes.Count} dupes - total retrieved = {result.Count} - unique = {unique.Count}");
+
+			Assert.Empty(dupes);
+		}
+
+		[Fact]
+		public async Task PagingTestAsync()
+		{
+			// This test fails if not ordering by ORDERBYsys_id
+			const string query = null;
+			var fieldList = new List<string>();
+			const string extraQueryString = null;
+
+			var result = await Client.GetAllByQueryAsync("u_ci_property", query, fieldList, extraQueryString, default).ConfigureAwait(false);
+			Assert.NotNull(result);
+			Assert.NotEmpty(result);
+			Assert.True(result[0].ContainsKey("sys_id"));
+			// Expecting the u_value field to be present as we didn't limit the fields to be retrieved
+			Assert.True(result[0].ContainsKey("u_value"));
 
 			// Check for dupes
 			var dupes = result.GroupBy(ci => ci["sys_id"]).Where(g => g.Count() > 1).Select(g => new { Id = g.First()["sys_id"], Count = g.Count() }).ToList();
@@ -61,10 +96,6 @@ namespace ServiceNow.Api.Test
 			var company = await Client.GetLinkedEntityAsync(companyLink, new List<string> { "name" }).ConfigureAwait(false);
 			Assert.NotNull(company);
 			Assert.NotNull(company["name"]);
-		}
-
-		public QueryTests(ITestOutputHelper output) : base(output)
-		{
 		}
 	}
 }
--- ServiceNow.Api.Test/ServiceNowTest.cs
+++ ServiceNow.Api.Test/ServiceNowTest.cs
@@ -8,58 +8,58 @@ using Xunit.Abstractions;
 
 namespace ServiceNow.Api.Test
 {
-	public abstract class ServiceNowTest
-	{
-		protected ILogger Logger { get; }
+	 public abstract class ServiceNowTest
+	 {
+		  protected ILogger Logger { get; }
 
-		/// <summary>
-		///   Constructs a ServiceNowClient
-		/// </summary>
-		/// <param name="iTestOutputHelper"></param>
-		/// <param name="appsettingsFilename"></param>
-		/// <param name="options"></param>
-		protected ServiceNowTest(ITestOutputHelper iTestOutputHelper, string appsettingsFilename = "appsettings.json", Options options = null)
-		{
-			options = options ?? new Options();
-			Logger = new LoggerFactory()
-				.AddDebug(LogLevel.Trace)
-				.AddXunit(iTestOutputHelper, LogLevel.Trace)
-				.CreateLogger<ServiceNowClient>();
-			options.Logger = Logger;
+		  /// <summary>
+		  ///   Constructs a ServiceNowClient
+		  /// </summary>
+		  /// <param name="iTestOutputHelper"></param>
+		  /// <param name="appsettingsFilename"></param>
+		  /// <param name="options"></param>
+		  protected ServiceNowTest(ITestOutputHelper iTestOutputHelper, string appsettingsFilename = "appsettings.json", Options options = null)
+		  {
+				options = options ?? new Options();
+				Logger = new LoggerFactory()
+					.AddDebug(LogLevel.Trace)
+					.AddXunit(iTestOutputHelper, LogLevel.Trace)
+					.CreateLogger<ServiceNowClient>();
+				options.Logger = Logger;
 
-			// Locate the configuration file path at the root of the test project, relative from where these assemblies were deployed
-			var configurationJsonFilePath = Path.Combine(Path.GetDirectoryName(typeof(ServiceNowTest).GetTypeInfo().Assembly.Location), "../../..");
-			var configurationRoot = new ConfigurationBuilder()
-				.SetBasePath(configurationJsonFilePath)
-				.AddJsonFile(appsettingsFilename, optional: false, reloadOnChange: false)
-				.Build();
-			var config = configurationRoot.Get<TestConfiguration>();
-			if (string.IsNullOrWhiteSpace(config.ServiceNowAccount))
-			{
-				throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowAccount)} must be set.");
-			}
+				// Locate the configuration file path at the root of the test project, relative from where these assemblies were deployed
+				var configurationJsonFilePath = Path.Combine(Path.GetDirectoryName(typeof(ServiceNowTest).GetTypeInfo().Assembly.Location), "../../..");
+				var configurationRoot = new ConfigurationBuilder()
+					.SetBasePath(configurationJsonFilePath)
+					.AddJsonFile(appsettingsFilename, optional: false, reloadOnChange: false)
+					.Build();
+				var config = configurationRoot.Get<TestConfiguration>();
+				if (string.IsNullOrWhiteSpace(config.ServiceNowAccount))
+				{
+					 throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowAccount)} must be set.");
+				}
 
-			if (string.IsNullOrWhiteSpace(config.ServiceNowUsername))
-			{
-				throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowUsername)} must be set.");
-			}
+				if (string.IsNullOrWhiteSpace(config.ServiceNowUsername))
+				{
+					 throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowUsername)} must be set.");
+				}
 
-			if (string.IsNullOrWhiteSpace(config.ServiceNowPassword))
-			{
-				throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowPassword)} must be set.");
-			}
+				if (string.IsNullOrWhiteSpace(config.ServiceNowPassword))
+				{
+					 throw new Exception($"{nameof(TestConfiguration)}.{nameof(TestConfiguration.ServiceNowPassword)} must be set.");
+				}
 
-			Client = new ServiceNowClient(
-				config.ServiceNowAccount,
-				config.ServiceNowUsername,
-				config.ServiceNowPassword,
-				options
-			);
-		}
+				Client = new ServiceNowClient(
+					config.ServiceNowAccount,
+					config.ServiceNowUsername,
+					config.ServiceNowPassword,
+					options
+				);
+		  }
 
-		/// <summary>
-		///    the client used by the test
-		/// </summary>
-		protected ServiceNowClient Client { get; }
-	}
+		  /// <summary>
+		  ///    the client used by the test
+		  /// </summary>
+		  protected ServiceNowClient Client { get; }
+	 }
 }
