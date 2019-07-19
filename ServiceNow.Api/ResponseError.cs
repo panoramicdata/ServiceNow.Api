89b62f4fe7d8249a46c1ccef5c8421e63a8395e7
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2019-07-19 14:30:12 +0100

    Added support for recognising status failure messages.

---
+++ ServiceNow.Api/ResponseError.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace ServiceNow.Api
+{
+
+	[DataContract]
+	public class ResponseError
+	{
+		[DataMember(Name = "message")]
+		public string Message { get; set; }
+
+		[DataMember(Name = "detail")]
+		public string Detail { get; set; }
+	}
+}
--- ServiceNow.Api/RestListResponseBase.cs
+++ ServiceNow.Api/RestListResponseBase.cs
@@ -1,7 +1,16 @@
-﻿namespace ServiceNow.Api
+﻿using System.Runtime.Serialization;
+
+namespace ServiceNow.Api
 {
+	[DataContract]
 	public abstract class RestListResponseBase
 	{
 		public int TotalCount { get; set; }
+
+		[DataMember(Name = "error")]
+		public ResponseError Error { get; set; }
+
+		[DataMember(Name = "status")]
+		public string Status { get; set; }
 	}
 }
